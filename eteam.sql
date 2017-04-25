b0234bc6fe3ff713532cf7d16dc7e78f54d61261
Scott Edenbaum <Scott.Edenbaum@gmail.com>
2017-04-25 15:19:11 -0400

    sql tables for sql learning

---
+++ eteam.sql
@@ -0,0 +1,18 @@
+CREATE TABLE eteam (id varchar(3) NULL,teamname varchar(50) NULL,coach varchar(50) NULL);
+
+INSERT INTO eteam VALUES ("POL","Poland","Franciszek Smuda");
+INSERT INTO eteam VALUES ("RUS","Russia","Dick Advocaat");
+INSERT INTO eteam VALUES ("CZE","Czech Republic","Michal Bílek");
+INSERT INTO eteam VALUES ("GRE","Greece","Fernando Santos");
+INSERT INTO eteam VALUES ("NED","Netherlands","Bert van Marwijk");
+INSERT INTO eteam VALUES ("DEN","Denmark","Morten Olsen");
+INSERT INTO eteam VALUES ("GER","Germany","Joachim Löw");
+INSERT INTO eteam VALUES ("POR","Portugal","Paulo Bento");
+INSERT INTO eteam VALUES ("ESP","Spain","Vicente del Bosque");
+INSERT INTO eteam VALUES ("ITA","Italy","Cesare Prandelli");
+INSERT INTO eteam VALUES ("IRL","Republic of Ireland","Giovanni Trapattoni");
+INSERT INTO eteam VALUES ("CRO","Croatia","Slaven Bilic");
+INSERT INTO eteam VALUES ("UKR","Ukraine","Oleh Blokhin");
+INSERT INTO eteam VALUES ("SWE","Sweden","Erik Hamrén");
+INSERT INTO eteam VALUES ("ENG","England","Roy Hodgson");
+INSERT INTO eteam VALUES ("FRA","France","Laurent Blanc");
